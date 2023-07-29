f30ccf63a020b6430cd97fece8a29989a2769aef
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-07-30 00:16:14 +0700

    Update test_registr_positive.py

--- tests/test_registr_positive.py
+++ tests/test_registr_positive.py
@@ -1,28 +1,28 @@
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from pages.registration_email import RegistrationEmail
 from pages.auth import *
-from selenium.webdriver.common.by import By
 from pages.settings import valid_phone, valid_login, valid_password, fake_firstname, fake_lastname, fake_password
-import time
-import pytest
-
 
 class TestRegistration:
 
-    """Проверка регистрации на сайте"""
+    #Проверка регистрации на сайте
 
-    # Выносим данные в тело класса для доступа к значениям переменных из всех функций класса:
+    #Выносим данные в тело класса для доступа к значениям переменных из всех функций класса:
 
     result_email, status_email = RegistrationEmail().get_api_email()  # запрос на получение валидного почтового ящика
-    email_reg = result_email[0]  # из запроса получаем валидный email
+    email_reg = result_email[0]  #из запроса получаем валидный email
 
     @pytest.mark.reg
     @pytest.mark.positive
 
     def test_get_registration_valid(self, browser):
 
-        """Валидный вариант регистрации при использовании email и получения кода для входа на почту.
-         Используем виртуальный почтовый ящик '1secmail.com' и получаем данные через GET запросы.
-         Добавляем созданный email в файл settings."""
+        #Валидный вариант регистрации при использовании email и получения кода для входа на почту.
+        #Используем виртуальный почтовый ящик и получаем данные через GET запросы.
+        #Добавляем созданный email в файл settings
 
         # Разделяем email на имя и домен для использования в следующих запросах:
         sign_at = self.email_reg.find('@')
@@ -37,44 +37,53 @@ class TestRegistration:
         page.enter_reg_page()
         browser.implicitly_wait(2)
         assert page.get_relative_link() == '/auth/realms/b2c/login-actions/registration'
-
         page = RegPage(browser)
+		
         # Вводим имя:
         page.enter_firstname(fake_firstname)
-        browser.implicitly_wait(5)
-        # Вводим фамилию:
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='firstname']")))
+       
+ 	    # Вводим фамилию:
         page.enter_lastname(fake_lastname)
-        browser.implicitly_wait(5)
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='lastname']")))
+		
         # Вводим адрес почты/Email:
         page.enter_email(self.email_reg)
-        browser.implicitly_wait(3)
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='email']")))
+		
         # Вводим пароль:
         page.enter_password(fake_password)
-        browser.implicitly_wait(3)
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='password']")))
+		
         # Вводим подтверждение пароля:
         page.enter_pass_conf(fake_password)
-        browser.implicitly_wait(3)
+        WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='pass_conf']")))
+		
         # Нажимаем на кнопку 'Зарегистрироваться':
         page.btn_click()
-        time.sleep(30)  # подождём, пока на почту придёт письмо...
+        WebDriverWait(browser, 30).until(EC.url_to_be("/account_b2c/page"))
 
-        """Проверяем почтовый ящик на наличие писем и достаём ID последнего письма"""
+        #Проверяем почтовый ящик на наличие писем и достаём ID последнего письма
         result_id, status_id = RegistrationEmail().get_id_letter(mail_name, mail_domain)
+		
         # Получаем id письма с кодом из почтового ящика:
         id_letter = result_id[0].get('id')
-        # Сверяем полученные данные с нашими ожиданиями
+		
+        # Сверяем полученные данные
         assert status_id == 200, "status_id error"
         assert id_letter > 0, "id_letter > 0 error"
 
-        """Получаем код регистрации из письма от Ростелекома"""
+        #Получаем код регистрации из письма от Ростелекома
         result_code, status_code = RegistrationEmail().get_reg_code(mail_name, mail_domain, str(id_letter))
 
-        # Получаем body из текста письма:
+        #Получаем body из текста письма:
         text_body = result_code.get('body')
-        # Извлекаем код из текста методом find:
+		
+        #Извлекаем код из текста методом find:
         reg_code = text_body[text_body.find('Ваш код : ') + len('Ваш код : '):
                              text_body.find('Ваш код : ') + len('Ваш код : ') + 6]
-        # Сверяем полученные данные с нашими ожиданиями
+        
+		#Сверяем полученные данные
         assert status_code == 200, "status_code error"
         assert reg_code != '', "reg_code != [] error"
 
@@ -85,13 +94,14 @@ class TestRegistration:
             browser.implicitly_wait(5)
         browser.implicitly_wait(30)
 
-        """Проверяем, что регистрация пройдена и пользователь перенаправлен в личный кабинет"""
+        #Проверяем, что регистрация пройдена и пользователь перенаправлен в личный кабинет
         assert page.get_relative_link() == '/account_b2c/page', 'Регистрация НЕ пройдена'
         page.driver.save_screenshot('reg_done.png')
 
-        """В случае успешной регистрации, перезаписываем созданные пару email/пароль в файл settings"""
+        #В случае успешной регистрации, перезаписываем созданные пару email/пароль в файл settings
         page.driver.save_screenshot('reg_done.png')
         print(self.email_reg, fake_password)
+		
         with open(r"../pages/Settings.py", 'r', encoding='utf8') as file:
             lines = []
             print(lines)
