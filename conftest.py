9e4635713d475f160073f7713f7ee71d2f25b30f
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-07-29 19:05:43 +0700

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,25 @@
-import time
 import pytest
-from selenium import webdriver
+import time
 from faker import Faker
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session')
 def browser():
-   driver = webdriver.Firefox(executable_path='geckodriver.exe')
+    """Инициализация браузера перед выполнением тестов"""
+    # Настройки FirefoxDriver
+    options = Options()
+    options.add_argument('-headless')  # Запуск браузера в режиме "без головы"
+
+    # Инициализация WebDriver
+    driver = webdriver.Firefox(options=options)
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+
+    # Переход к странице авторизации
+    driver.get('https://b2c.passport.rt.ru')
 
     yield driver
+
+    # Завершение работы WebDriver после выполнения тестов
     driver.quit()
