7fd8629c7dc19dd3906627bebf170cb607704aae
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-07-30 02:22:22 +0700

    Update test_auth_page_negative.py

--- tests/test_auth_page_negative.py
+++ tests/test_auth_page_negative.py
@@ -1,7 +1,6 @@
+import pytest
 import ast
 import pickle
-import time
-import pytest
 from pages.auth import *
 from pages.settings import *
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,8 +13,7 @@ from selenium.webdriver.common.by import By
 @pytest.mark.parametrize('username', [fake_phone, fake_login, invalid_ls],
                          ids=['fake phone', 'fake login', 'fake service account'])
 def test_auth_page_fake_phone_login_serv_account(browser, username):
-    """ Проверка авторизации по номеру телефона и паролю, лицевому счету,
-    неверный номер/логин/лицевой счет."""
+    #Проверка авторизации по номеру телефона и паролю, лицевому счету, неверный номер/логин/лицевой счет.
     page = AuthPage(browser)
     page.enter_username(username)
     page.enter_password(valid_password)
@@ -32,7 +30,7 @@ def test_auth_page_fake_phone_login_serv_account(browser, username):
 @pytest.mark.auth
 @pytest.mark.negative
 def test_auth_page_fake_email(browser):
-    """Проверка авторизации по почте и паролю, неверная почта"""
+    #Проверка авторизации по почте и паролю, неверная почта
     page = AuthPage(browser)
     page.enter_username(fake_email)
     page.enter_password(valid_pass_reg)
@@ -54,8 +52,7 @@ def test_auth_page_fake_email(browser):
 @pytest.mark.parametrize('username', [valid_phone, valid_email, valid_login],
                          ids=['valid phone', 'valid login', 'valid email'])
 def test_auth_page_fake_password(browser, username):
-    """Проверка авторизации по номеру телефона/почте/логину и паролю. Неверный пароль.
-    Тест по ЛС и паролю не проводился из-за отсутствия реальных тестовых данных"""
+    #Проверка авторизации по номеру телефона/почте/логину и паролю. Неверный пароль.
     page = AuthPage(browser)
     page.enter_username(username)
     page.enter_password(fake_password)
@@ -72,7 +69,7 @@ def test_auth_page_fake_password(browser, username):
 @pytest.mark.auth
 @pytest.mark.negative
 def test_auth_page_phone_empty_username(browser):
-    """Проверка авторизации по телефону, пустой номер"""
+    #Проверка авторизации по телефону, пустой номер
     page = AuthPage(browser)
     page.clear_username_field()
     page.enter_password(valid_pass_phone)
@@ -89,7 +86,7 @@ def test_auth_page_phone_empty_username(browser):
 @pytest.mark.parametrize('username', [1, 111111111],
                          ids=['one digit', '9 digits'])
 def test_auth_page_invalid_username(browser, username):
-    """Проверка авторизации по номеру телефона и паролю, неверный формат телефона"""
+    #Проверка авторизации по номеру телефона и паролю, неверный формат телефона
     page = AuthPage(browser)
     page.enter_username(username)
     page.enter_password(valid_password)
