65f769f1ac8ae1530235eef5d07f1e7b4852cc35
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-07-30 02:16:13 +0700

    Update settings.py

--- pages/settings.py
+++ pages/settings.py
@@ -1,12 +1,10 @@
-"""Действующие данные для авторизации в системе"""
 import os
 from dotenv import load_dotenv
 from faker import Faker
 import string
 load_dotenv()
 
-
-"""Фейковые данные для авторизации в системе"""
+#Фейковые данные для авторизации в системе
 fake_ru = Faker('ru_RU')
 fake_firstname = fake_ru.first_name()
 fake_lastname = fake_ru.last_name()
@@ -16,35 +14,28 @@ fake_password = fake.password()
 fake_login = fake.user_name()
 fake_email = fake.email()
 
-
+#Действующие данные для авторизации в системе
 valid_phone = os.getenv('phone')
 valid_login = os.getenv('login')
 valid_password = os.getenv('password')
 invalid_ls = '241910001343'
-
 valid_email = 'cepapov192@quipas.com'
 valid_pass_reg = '!O$O&veew0'
-
 
 def generate_string_rus(n):
     return 'б' * n
 
-
 def generate_string_en(n):
     return 'x' * n
-
 
 def english_chars():
     return 'qwertyuiopasdfghjklzxcvbnm'
 
-
 def russian_chars():
     return 'абвгдеёжзиклмнопрстуфхцчшщъыьэюя'
 
-
 def chinese_chars():    # 20 популярных китайских иероглифов
     return '的一是不了人我在有他这为之大来以个中上们'
-
 
 def special_chars():
     return f'{string.punctuation}'
